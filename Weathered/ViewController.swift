57bc5152dbbae54826b754177a5d7adebb9f1290
Rob Elser <Rob@biomeshstudios.com>
2017-10-31 11:33:30 +0000

    Initial Commit

---
+++ Weathered/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weathered
+//
+//  Created by Robert Elser on 31/10/2017.
+//  Copyright © 2017 Biomesh Studios, Ltd. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
