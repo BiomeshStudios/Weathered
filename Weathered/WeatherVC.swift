dfcd4066d84665c87e3d189f6f55cce518dee94a
Rob Elser <Rob@biomeshstudios.com>
2017-10-31 14:02:10 +0000

    WeatherVC
    created initial ViewController layout

--- Weathered/WeatherVC.swift
+++ Weathered/WeatherVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherVC.swift
 //  Weathered
 //
 //  Created by Robert Elser on 31/10/2017.
@@ -8,14 +8,23 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class WeatherVC: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
 
+//    override func viewWillAppear() {
+//        <#code#>
+//    }
+    
+    override func viewDidAppear() {
+        self.view.layer?.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
